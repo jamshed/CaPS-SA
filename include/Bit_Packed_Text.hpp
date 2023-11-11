15ba7622dbbed324f3c5f96dd81b5389916df71b
jamshed <jamshed@umd.edu>
2023-11-11 13:05:39 -0500

    Add bit-packed rep construction

---
+++ include/Bit_Packed_Text.hpp
@@ -0,0 +1,43 @@
+
+#ifndef BIT_PACKED_TEXT_HPP
+#define BIT_PACKED_TEXT_HPP
+
+
+
+#include "utilities.hpp"
+
+#include <cstdint>
+#include <cstddef>
+
+
+// =============================================================================
+
+namespace CaPS_SA
+{
+
+// A class to to represent genomic texts in a 2-bit-packed manner.
+class Bit_Packed_Text
+{
+private:
+
+    const char* const T;    // The input text.
+    const std::size_t n;    // Length of the input text.
+
+    const std::size_t pack_sz;  // Size of the bit-pack.
+    uint8_t* const B;   // The bit-packed representation.
+
+
+public:
+
+    // Constructs an object for 2-bit-packing a genomic text `T` of length `n`.
+    Bit_Packed_Text(const char* T = nullptr, std::size_t n = 0);
+
+    // Constructs the bit-packed representation.
+    void construct();
+};
+
+}
+
+
+
+#endif
---
+++ src/Bit_Packed_Text.cpp
@@ -0,0 +1,47 @@
+
+#include "Bit_Packed_Text.hpp"
+#include "parlay/parallel.h"
+
+
+namespace CaPS_SA
+{
+
+Bit_Packed_Text::Bit_Packed_Text(const char* T, std::size_t n):
+      T(T)
+    , n(n)
+    , pack_sz((n + 3) / 4)
+    , B(T != nullptr ? allocate<uint8_t>(pack_sz) : nullptr)
+{}
+
+
+void Bit_Packed_Text::construct()
+{
+    const auto base_code =
+        [](char ch)
+        {
+            ch &= ~32;  // To upper-case.
+            assert(ch == 'A' || ch == 'C' || ch == 'G' || ch == 'T');
+
+            return ((ch >> 2) ^ (ch >> 1)) & 0b11;
+        };
+
+    const auto pack =
+        [&](const std::size_t i)
+        {
+            B[i]  = (base_code(T[i << 2]) << 6) |
+                    (base_code(T[(i << 2) | 0b01]) << 4) |
+                    (base_code(T[(i << 2) | 0b10]) << 2) |
+                    (base_code(T[(i << 2) | 0b11]));
+        };
+
+
+    parlay::parallel_for(0, n / 4, pack);
+    if(n & 3)
+    {
+        B[n / 4] = 0;
+        for(std::size_t i = 0; i < (n & 3); ++i)
+            B[n / 4] |= (base_code(T[(n / 4) * 4 + i]) << ((3 - i) * 2));
+    }
+}
+
+}
