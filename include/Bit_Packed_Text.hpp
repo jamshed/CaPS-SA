97c25998ff6582fad3f662982c04cbcdd32e51a4
jamshed <jamshed@umd.edu>
2023-11-11 22:19:45 -0500

    Add load from bit-pack

--- include/Bit_Packed_Text.hpp
+++ include/Bit_Packed_Text.hpp
@@ -8,6 +8,8 @@
 
 #include <cstdint>
 #include <cstddef>
+#include <immintrin.h>
+#include <cassert>
 
 
 // =============================================================================
@@ -27,6 +29,11 @@ private:
     uint8_t* const B;   // The bit-packed representation.
 
 
+    // Returns the 124-nucleobase block from the `i` 'th character, in 256-bits
+    // little-endian. No guarantees are provided on the highest byte.
+    __m256i load(std::size_t idx);
+
+
 public:
 
     // Constructs an object for 2-bit-packing a genomic text `T` of length `n`.
@@ -39,6 +46,29 @@ public:
     // indices and bits.
     void print();
 };
+
+
+inline __m256i Bit_Packed_Text::load(const std::size_t i)
+{
+    assert(i + 124 <= n);
+
+    const auto base_idx = i / 4;    // Base word's index.
+    const auto blk = _mm256_loadu_si256(reinterpret_cast<const __m256i*>(B + base_idx));
+
+    const auto base_trail = (i & 3);    // Number of unwanted bases trailing in the base word.
+    if(!base_trail)
+        return blk;
+
+
+    const auto to_clear_r = _mm256_set1_epi64x(base_trail * 2);
+    const auto trail_cleared = _mm256_srlv_epi64(blk, to_clear_r);
+    const auto r_shifted_words = _mm256_permute4x64_epi64(blk, 0b11'11'10'01);
+    const auto to_clear_l = _mm256_set1_epi64x((32 - base_trail) * 2);
+    const auto lost_bits = _mm256_sllv_epi64(r_shifted_words, to_clear_l);
+    const auto restored = _mm256_or_si256(trail_cleared, lost_bits);
+
+    return restored;
+}
 
 }
 
