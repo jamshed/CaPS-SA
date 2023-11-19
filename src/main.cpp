3b9bf52570b8bcf73e94e9ded5720c164704a0dc
Rob Patro <rob-p@github.com>
2023-11-18 19:33:35 -0500

    make conversion of input string optional

--- src/main.cpp
+++ src/main.cpp
@@ -51,11 +51,14 @@ int main(int argc, char* argv[])
     std::string op_path;
     std::size_t subproblem_count{0};
     std::size_t max_context{0};
+    bool convert_chars{false};
 
     app.add_option("-i,--input", ip_path, "input file on which to build sa")->required();
     app.add_option("-o,--output", op_path, "ouput file where SA should be written");
     app.add_option("-c,--context", max_context, "bounded context length")->capture_default_str();
     app.add_option("-s,--subprob-count", subproblem_count, "subproblem count")->capture_default_str();
+    app.add_flag("--convert-chars", convert_chars, "If the input string may contain characters other "
+                 "than A,C,G,T, pass this to convert the string before building the suffix array.");
     CLI11_PARSE(app, argc, argv);
 
     std::string text;
@@ -64,14 +67,16 @@ int main(int argc, char* argv[])
     // the actual size of the text is 8 less than 
     // text.length() because of the padding.
     std::size_t n = text.length() - 8;
-    parlay::blocked_for(0, n, 65536, 
-      [&, n](size_t i, size_t start, size_t end) {
-        (void)i;
-        for (size_t j = start; j < std::min(end, n); ++j) {
-          char c = text[j];
-          text[j] = lookup[((std::toupper(c) & 0x6) >> 1)];
-        };
-    });
+    if (convert_chars) {
+      parlay::blocked_for(0, n, 65536, 
+        [&, n](size_t i, size_t start, size_t end) {
+          (void)i;
+          for (size_t j = start; j < std::min(end, n); ++j) {
+            char c = text[j];
+            text[j] = lookup[((std::toupper(c) & 0x6) >> 1)];
+          };
+      });
+    }
 
     std::ofstream output(op_path);
 
